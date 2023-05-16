4196ef3a47e77530bc099dc01dbf0fdc7e563429
Elisey Esaulkov <toelisey@yandex.ru>
2023-05-16 10:44:51 +0500

    fix(backend): add dev-secret

--- backend/appConfig.js
+++ backend/appConfig.js
@@ -1,7 +1,9 @@
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET_PRODUCTION } = process.env;
 const {
   JOI_PRESETS,
 } = require('./utils/joi-preset-validation-rules');
+
+const JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET_PRODUCTION : 'dev-secret';
 
 module.exports = {
   JWT_SECRET,
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 export const API_HEADERS = {
-  authorization: 'e7f60b85-9e30-4602-89c2-652f79ad0c99',
+  // authorization: 'e7f60b85-9e30-4602-89c2-652f79ad0c99',
 }
 export const API_BASE_URL = 'mesto.elisey.students.nomoredomains.monster';
